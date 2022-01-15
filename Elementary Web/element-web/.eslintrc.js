c3362f1144a17a37a2b89c2af2b72325571bf941
Quang Lê <quanggr8@gmail.com>
2022-01-16 01:14:54 +0700

    update eslint

--- Elementary Web/element-web/.eslintrc.js
+++ Elementary Web/element-web/.eslintrc.js
@@ -1,30 +1,33 @@
 module.exports = {
     plugins: ["matrix-org"],
-    extends: [
-        "plugin:matrix-org/babel",
-        "plugin:matrix-org/react",
-    ],
+    extends: ["plugin:matrix-org/babel", "plugin:matrix-org/react"],
     env: {
         browser: true,
         node: true,
     },
     rules: {
         // Things we do that break the ideal style
-        "quotes": "off",
+        quotes: "off",
     },
-    overrides: [{
-        files: ["src/**/*.{ts,tsx}"],
-        extends: [
-            "plugin:matrix-org/typescript",
-            "plugin:matrix-org/react",
-        ],
-        rules: {
-            // Things we do that break the ideal style
-            "prefer-promise-reject-errors": "off",
-            "quotes": "off",
+    overrides: [
+        {
+            files: ["src/**/*.{ts,tsx}"],
+            extends: [
+                "plugin:matrix-org/typescript",
+                "plugin:matrix-org/react",
+            ],
+            rules: {
+                // Things we do that break the ideal style
+                "prefer-promise-reject-errors": "off",
+                quotes: "off",
+                "linebreak-style": 0,
 
-            // We disable this while we're transitioning
-            "@typescript-eslint/no-explicit-any": "off",
+                // We disable this while we're transitioning
+                "@typescript-eslint/no-explicit-any": "off",
+            },
         },
-    }],
+    ],
+    parser: "@babel/eslint-parser",
+    parserOptions: { requireConfigFile: "false" },
+    babelOptions: { configFile: "./.babelrc" },
 };
